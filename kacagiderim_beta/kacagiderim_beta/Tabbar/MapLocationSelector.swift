8c98a466953e0dbe1662b23d1d720437ca5d6218
kamili <kamil.inal@gmail.com>
2018-09-07 18:27:11 +0300

    MapLocationSelector added

---
+++ kacagiderim_beta/kacagiderim_beta/Tabbar/MapLocationSelector.swift
@@ -0,0 +1,28 @@
+//
+//  MapLocationSelector.swift
+//  kacagiderim_beta
+//
+//  Created by Comodo on 7.09.2018.
+//  Copyright © 2018 kacagiderim. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MapLocationSelector : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
