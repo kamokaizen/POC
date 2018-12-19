c07f079be4152ab879fee55abb6f24bb8305617b
kamili <kamili@comodo.net>
2018-12-19 19:04:00 +0300

    spring boot project updates.

--- Spring-Boot/app/src/main/java/com/exampl/myproject/Application.java
+++ Spring-Boot/app/src/main/java/com/exampl/myproject/Application.java
@@ -17,69 +17,73 @@ import java.util.Collection;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
-@SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
+@SpringBootApplication // same as @Configuration @EnableAutoConfiguration
+						// @ComponentScan
 public class Application {
 
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
-	
-    @Inject
-    private Environment env;
-	
-    /**
-     * Initializes app.
-     * <p>
-     * Spring profiles can be configured with a program arguments --spring.profiles.active=your-active-profile
-     * <p>
-     */
-    @PostConstruct
-    public void initApplication() {
-        if (env.getActiveProfiles().length == 0) {
-            log.warn("No Spring profile configured, running with default configuration");
-        } else {
-            log.info("Running with Spring profile(s) : {}", Arrays.toString(env.getActiveProfiles()));
-            Collection<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
-            if (activeProfiles.contains(Constants.SPRING_PROFILE_DEVELOPMENT) && activeProfiles.contains(Constants.SPRING_PROFILE_PRODUCTION)) {
-                log.error("You have misconfigured your application! " +
-                    "It should not run with both the 'dev' and 'prod' profiles at the same time.");
-            }
-            if (activeProfiles.contains(Constants.SPRING_PROFILE_DEVELOPMENT) && activeProfiles.contains(Constants.SPRING_PROFILE_CLOUD)) {
-                log.error("You have misconfigured your application! " +
-                    "It should not run with both the 'dev' and 'cloud' profiles at the same time.");
-            }
-        }
-    }
-    
-    /**
-     * Main method, used to run the application.
-     *
-     * @param args the command line arguments
-     * @throws UnknownHostException if the local host name could not be resolved into an address
-     */
-    public static void main(String[] args) throws UnknownHostException {
-        SpringApplication app = new SpringApplication(Application.class);
-        SimpleCommandLinePropertySource source = new SimpleCommandLinePropertySource(args);
-        addDefaultProfile(app, source);
-        Environment env = app.run(args).getEnvironment();
-        log.info("\n----------------------------------------------------------\n\t" +
-                "Application '{}' is running! Access URLs:\n\t" +
-                "Local: \t\thttp://127.0.0.1:{}\n\t" +
-                "External: \thttp://{}:{}\n----------------------------------------------------------",
-            env.getProperty("spring.application.name"),
-            env.getProperty("server.port"),
-            InetAddress.getLocalHost().getHostAddress(),
-            env.getProperty("server.port"));
 
-    }
+	@Inject
+	private Environment env;
 
-    /**
-     * If no profile has been configured, set by default the "dev" profile.
-     */
-    private static void addDefaultProfile(SpringApplication app, SimpleCommandLinePropertySource source) {
-        if (!source.containsProperty("spring.profiles.active") &&
-                !System.getenv().containsKey("SPRING_PROFILES_ACTIVE")) {
+	/**
+	 * Initializes app.
+	 * <p>
+	 * Spring profiles can be configured with a program arguments
+	 * --spring.profiles.active=your-active-profile
+	 * <p>
+	 */
+	@PostConstruct
+	public void initApplication() {
+		if (env.getActiveProfiles().length == 0) {
+			log.warn("No Spring profile configured, running with default configuration");
+		} else {
+			log.info("Running with Spring profile(s) : {}", Arrays.toString(env.getActiveProfiles()));
+			Collection<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
+			if (activeProfiles.contains(Constants.SPRING_PROFILE_DEVELOPMENT)
+					&& activeProfiles.contains(Constants.SPRING_PROFILE_PRODUCTION)) {
+				log.error("You have misconfigured your application! "
+						+ "It should not run with both the 'dev' and 'prod' profiles at the same time.");
+			}
+			if (activeProfiles.contains(Constants.SPRING_PROFILE_DEVELOPMENT)
+					&& activeProfiles.contains(Constants.SPRING_PROFILE_CLOUD)) {
+				log.error("You have misconfigured your application! "
+						+ "It should not run with both the 'dev' and 'cloud' profiles at the same time.");
+			}
+		}
+	}
 
-            app.setAdditionalProfiles(Constants.SPRING_PROFILE_DEVELOPMENT);
-        }
-    }
-    
+	/**
+	 * Main method, used to run the application.
+	 *
+	 * @param args
+	 *            the command line arguments
+	 * @throws UnknownHostException
+	 *             if the local host name could not be resolved into an address
+	 */
+	public static void main(String[] args) throws UnknownHostException {
+		SpringApplication app = new SpringApplication(Application.class);
+		SimpleCommandLinePropertySource source = new SimpleCommandLinePropertySource(args);
+		addDefaultProfile(app, source);
+		Environment env = app.run(args).getEnvironment();
+		log.info(
+				"\n----------------------------------------------------------\n\t"
+						+ "Application '{}' is running! Access URLs:\n\t" + "Local: \t\thttp://127.0.0.1:{}\n\t"
+						+ "External: \thttp://{}:{}\n----------------------------------------------------------",
+				env.getProperty("spring.application.name"), env.getProperty("server.port"),
+				InetAddress.getLocalHost().getHostAddress(), env.getProperty("server.port"));
+
+	}
+
+	/**
+	 * If no profile has been configured, set by default the "dev" profile.
+	 */
+	private static void addDefaultProfile(SpringApplication app, SimpleCommandLinePropertySource source) {
+		if (!source.containsProperty("spring.profiles.active")
+				&& !System.getenv().containsKey("SPRING_PROFILES_ACTIVE")) {
+
+			app.setAdditionalProfiles(Constants.SPRING_PROFILE_DEVELOPMENT);
+		}
+	}
+
 }
