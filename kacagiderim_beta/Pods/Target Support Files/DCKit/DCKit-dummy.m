04d21253faa8bc883fe6ad3b8afcc298cf95e64d
kamili <kamili@comodo.net>
2018-12-25 10:36:38 +0300

    The project removed.

--- kacagiderim_beta/Pods/Target Support Files/DCKit/DCKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DCKit : NSObject
-@end
-@implementation PodsDummy_DCKit
-@end
--- kacagiderim_beta/Pods/Target Support Files/Pods-kacagiderim_beta/Pods-kacagiderim_beta-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_kacagiderim_beta : NSObject
-@end
-@implementation PodsDummy_Pods_kacagiderim_beta
-@end
